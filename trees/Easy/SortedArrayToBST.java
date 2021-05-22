033124480fbbd1d18b9a99cbd5232a2b59698f9a
Sahithi <62688362+Gameon99@users.noreply.github.com>
2021-05-22 19:46:25 +0530

    Create SortedArrayToBST.java

---
+++ trees/Easy/SortedArrayToBST.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        int length = nums.length;
+        if(length == 0){
+            return null;
+        }
+        return buildBST(nums,0,length-1);
+    }
+    
+    public TreeNode buildBST(int[] nums,int start,int end){
+        if(start > end){
+            return null;
+        }
+        int mid = (start + end)/2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = buildBST(nums,start,mid-1);
+        root.right = buildBST(nums,mid+1,end);
+        return root;
+    }
+}
