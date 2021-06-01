b7ad24e52b035a4b9abd65930caafa002122bea6
Sahithi <62688362+Gameon99@users.noreply.github.com>
2021-06-01 19:04:53 +0530

    Update climbingStairs.java

--- dp/climbingStairs.java
+++ dp/climbingStairs.java
@@ -10,4 +10,22 @@ class Solution {
 }
 
 //dp solution
+class Solution {
+    public int climbStairs(int n) {
+        if(n <= 2){
+            return n;
+        }
+        
+        int[] count = new int[n+1];
+        
+        for(int i = 0;i <= 2;i++){
+            count[i] = i;
+        }
+        for(int i = 3;i <= n;i++){
+            count[i] = count[i-1] + count[i-2];
+        }
+        return count[n];
+        
+    }
+}
 
